eef351b41632ad597506155cfb1f0d3d7eb8c7dc
Sjors Holtrop <sjorsholtrop@hotmail.com>
2020-01-31 16:43:45 +0100

    change db connection string

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,8 +4,7 @@ export function setupDatabase() {
   mongoose.set("useFindAndModify", false);
   mongoose.set("useCreateIndex", true);
   mongoose.set("useUnifiedTopology", true);
-  const connectionString =
-    process.env.DATABASE_URL || "mongodb://localhost/boterbot";
+  const connectionString = "mongodb://localhost:27017";
   mongoose.connect(connectionString, { useNewUrlParser: true });
   const db = mongoose.connection;
   db.on("error", console.error.bind(console, "Mongoose - connection error"));
